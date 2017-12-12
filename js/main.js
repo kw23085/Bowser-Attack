13a80a1dbffb6809f49ec6259d75ec48c9c39e30
Kevin Wang <kw1017@gmail.com>
2017-12-12 09:33:33 -0800

    created boss and added animation

--- js/main.js
+++ js/main.js
@@ -1,18 +1,74 @@
+var $body = $('body')
+
+// creates boss
+function startMovingBoss() {
+    var $newBoss = $('<div>')
+    $newBoss.addClass('boss')
+    var randomTop = Math.floor(Math.random() * (window.innerHeight - 150))
+    var randomLeft = Math.floor(Math.random() *(window.innerWidth -150))
+    $newBoss.css({
+        top: randomTop,
+        left: randomLeft,
+    })
+
+    // animate to move the boss around
+function moveBoss(boss) {
+     boss.animate({
+         top: Math.floor(Math.random() * (window.innerHeight - 150)),
+         left: Math.floor(Math.random() * (window.innerWidth - 150)),
+     }, 2000, function() {
+         moveBoss(boss)
+})
+}
+
+   // append boss div to body
+   $body.append($newBoss)
+   moveBoss($newBoss)
+}
+
+
+// fireball effect
 function enableFireballs() {
     $('body').on('mouseenter', '.fireball', function() {
         console.log("Ouch")
     })
 }
 
+// turn off fireball effect
 function disableFireballs() {
-    $('body').off('mouseenter')
+    $('body').off('mouseenter', '.fireball')
 }
 
-$('body').on('click', '.star', function() {
+// what happens after you get star
+$('body').on('mouseenter', '.star', function() {
     disableFireballs()
     setTimeout(function() {
         enableFireballs()
-    }, 5000)
+    }, 10000)
+    $(this).remove()
 })
+
+// generate fireball
+function shootFireBalls() {
+    var $newFireBall = $('<div>')
+    $newFireBalls.addClass('fireball')
+}
+
+// generate star at random location
+function generateStar() {
+    var $newStar = $('<div>')
+    $newStar.addClass('star')
+
+    var randomTop = Math.floor(Math.random() * (window.innerHeight - 50))
+    var randomLeft = Math.floor(Math.random() * (window.innerWidth - 50))
+
+    $newStar.css({
+        top: randomTop,
+        left: randomLeft,
+    })
+
+    $body.append($newStar)
+}
+    
 
 enableFireballs()
